9bd09187fc63328fd464f12fbcffeda5e1096529
TANADADaisuke <nvkx873110@gmail.com>
2021-07-22 20:12:19 +0900

    feat: Use Service Worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -38,6 +39,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "[name].css"
-        })
+        }),
+        new WorkboxPlugin.GenerateSW(),
     ]
 };
